23786d6cdbe6324b4bcb7eccc5a6c440ab55e5c2
Nat Lee <natlee.work@gmail.com>
2023-03-07 13:50:53 +0800

    [update] set id

--- src/content.js
+++ src/content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    var extension_id = chrome.runtime.id;
     if (request.greeting === "hello") {
         const s = document.createElement("script");
-        s.setAttribute("src", "chrome-extension://ggibadcfmilllfefhfbpifbamabehfdd/enable.js"); // chrome extension store
-        //s.setAttribute("src", "chrome-extension://ohkfmkndnoojnbdmkcnjphldllfggflo/enable.js"); // develop
+        s.setAttribute("src", "chrome-extension://" + extension_id + "/enable.js");
         document.documentElement.appendChild(s)
     }
 });
