3a1de3c5d8cb338e3e61616b839c5d922a904122
Nat Lee <natlee.work@gmail.com>
2023-03-09 11:09:10 +0800

    [update] add icons into zip

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm package.zip
 cp ./src/manifest.json ./manifest.json
-zip -FSr package.zip ./manifest.json ./src.crx
+cp -r ./src/icons ./
+zip -FSr package.zip ./manifest.json ./src.crx ./icons
 rm ./manifest.json
 rm src.crx src.pem
+rm -r ./icons
