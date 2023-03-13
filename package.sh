caf189975cee1c4b1787b483c99337c29bd8f4fc
Nat Lee <natlee.work@gmail.com>
2023-03-13 11:34:05 +0800

    [update] add js into zip

--- package.sh
+++ package.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 rm package.zip
 cp ./src/manifest.json ./manifest.json
+cp ./src/background.js ./background.js
+cp ./src/content.js ./content.js
+cp ./src/enable.js ./enable.js
 cp -r ./src/icons ./
-zip -FSr package.zip ./manifest.json ./src.crx ./icons
+zip -FSrj package.zip ./manifest.json ./src.crx ./icons
 rm ./manifest.json
+rm ./background.js
+rm ./content.js
+rm ./enable.js
 rm src.crx src.pem
 rm -r ./icons
